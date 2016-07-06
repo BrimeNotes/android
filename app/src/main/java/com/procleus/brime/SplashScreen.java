63bc6bc03d772b51f7ce517e74d1cac13fc43bab
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2016-07-06 14:31:44 +0530

    Minor Fix

--- app/src/main/java/com/procleus/brime/SplashScreen.java
+++ app/src/main/java/com/procleus/brime/SplashScreen.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.content.SharedPreferences;
 import android.transition.Fade;
 
 public class SplashScreen extends Activity {
