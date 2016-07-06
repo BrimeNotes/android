273356586a6a6c908b6c338192daef6b54aaabc3
maxsam4 <guptamudit@ymail.com>
2016-07-06 17:54:11 +0530

    signup completed optimized imports, rearanged code, reformat code, clean up (all by android studio)

--- app/src/main/java/com/procleus/brime/SignupActivity.java
+++ app/src/main/java/com/procleus/brime/SignupActivity.java
@@ -3,47 +3,38 @@ package com.procleus.brime;
 /**
  * Created by suraj on 04-07-2016.
  */
+
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
 
 public class SignupActivity extends AppCompatActivity {
 
     edittext etname, etemail, etpass;
     buttons btnsign;
     textview loginlink;
-    String link = new String("http://brime.tk/");
     private GoogleApiClient client;
 
     //Added hash function from maxsam4 commit
@@ -59,64 +50,8 @@ public class SignupActivity extends AppCompatActivity {
         }
     }
 
-    public String performPostCall(String requestURL,
-                                  HashMap<String, String> postDataParams) {
-        URL url;
-        String response = "";
-        try {
-            url = new URL(requestURL);
-
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setReadTimeout(15000);
-            conn.setConnectTimeout(15000);
-            conn.setRequestMethod("GET");
-            conn.setDoInput(true);
-            conn.setDoOutput(true);
 
 
-            OutputStream os = conn.getOutputStream();
-            BufferedWriter writer = new BufferedWriter(
-                    new OutputStreamWriter(os, "UTF-8"));
-            writer.write(getPostDataString(postDataParams));
-
-            writer.flush();
-            writer.close();
-            os.close();
-            int responseCode = conn.getResponseCode();
-
-            if (responseCode == HttpsURLConnection.HTTP_OK) {
-                String line;
-                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                while ((line = br.readLine()) != null) {
-                    response += line;
-                }
-            } else {
-                response = "";
-
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return response;
-
-    }
-
-    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
-        StringBuilder result = new StringBuilder();
-        boolean first = true;
-        for(Map.Entry<String, String> entry : params.entrySet()){
-            if (first)
-                first = false;
-            else
-                result.append("&");
-            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-        }
-
-        return result.toString();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -160,16 +95,10 @@ public class SignupActivity extends AppCompatActivity {
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Creating Account");
         progressDialog.show();
-        String name = etname.getText().toString();
-        String email = etemail.getText().toString();
-        String password = etpass.getText().toString();
+        //String name = etname.getText().toString();
 
-        HashMap<String,String> signupData = new HashMap<String,String>();
-        signupData.put("email",email);
-        signupData.put("p",encryptSHA512(password));
-        String response = new String();
-        response = performPostCall(link,signupData);
-        Log.i("response",response);
+        new PostClass(this).execute();
+
         new Handler().postDelayed(
                 new Runnable() {
                     public void run() {
@@ -227,43 +156,53 @@ public class SignupActivity extends AppCompatActivity {
         btnsign.setEnabled(true);
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
+    private class PostClass extends AsyncTask<String, Void, Void> {
 
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Signup Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.procleus.brime/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
+        private final Context context;
+        String email = etemail.getText().toString();
+        String password = etpass.getText().toString();
+
+        public PostClass(Context c) {
+
+            this.context = c;
+        }
+
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                URL url = new URL("http://brime.tk/register.php");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                String urlParameters = "email=" + URLEncoder.encode(email, "UTF-8") + "&p=" + URLEncoder.encode(encryptSHA512(password), "UTF-8");
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("USER-AGENT", "Brime Android App");
+                connection.setRequestProperty("ACCEPT-LANGUAGE", "en-US,en;0.5");
+                connection.setDoOutput(true);
+                DataOutputStream dStream = new DataOutputStream(connection.getOutputStream());
+                dStream.writeBytes(urlParameters);
+                dStream.flush();
+                dStream.close();
+                //int responseCode = connection.getResponseCode();
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+                StringBuilder responseOutput = new StringBuilder();
+                while ((line = br.readLine()) != null) {
+                    responseOutput.append(line);
+                }
+                Log.i("response", responseOutput.toString());
+                br.close();
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Signup Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.procleus.brime/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
