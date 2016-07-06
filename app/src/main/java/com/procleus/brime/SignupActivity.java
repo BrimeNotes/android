49625084fbd496f282c57c292b54e9ac680f5211
maxsam4 <guptamudit@ymail.com>
2016-07-06 11:04:55 +0530

    Signup funtion v 0.1

--- app/src/main/java/com/procleus/brime/SignupActivity.java
+++ app/src/main/java/com/procleus/brime/SignupActivity.java
@@ -6,44 +6,128 @@ package com.procleus.brime;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class SignupActivity extends AppCompatActivity {
 
-    edittext etname,etemail,etpass;
+    edittext etname, etemail, etpass;
     buttons btnsign;
     textview loginlink;
+    String link = new String("http://brime.tk/");
+    private GoogleApiClient client;
+
     //Added hash function from maxsam4 commit
-     public final static String encryptSHA512(String target) {
-             try {
-                    MessageDigest sh = MessageDigest.getInstance("SHA-512");
-                    sh.update(target.getBytes());
-                    StringBuffer sb = new StringBuffer();
-                    for (byte b : sh.digest()) sb.append(Integer.toHexString(0xff & b));
-                    return sb.toString();
-                 } catch (NoSuchAlgorithmException e) {
-                     throw new RuntimeException(e);
-                 }
+    public final static String encryptSHA512(String target) {
+        try {
+            MessageDigest sh = MessageDigest.getInstance("SHA-512");
+            sh.update(target.getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (byte b : sh.digest()) sb.append(Integer.toHexString(0xff & b));
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
-    
+
+    public String performPostCall(String requestURL,
+                                  HashMap<String, String> postDataParams) {
+        URL url;
+        String response = "";
+        try {
+            url = new URL(requestURL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(
+                    new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                while ((line = br.readLine()) != null) {
+                    response += line;
+                }
+            } else {
+                response = "";
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return response;
+
+    }
+
+    private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for(Map.Entry<String, String> entry : params.entrySet()){
+            if (first)
+                first = false;
+            else
+                result.append("&");
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        }
+
+        return result.toString();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         // Binding resources
-        etname=(edittext)findViewById(R.id.input_name);
-        etemail=(edittext)findViewById(R.id.input_email);
-        etpass=(edittext)findViewById(R.id.input_password);
-        loginlink=(textview)findViewById(R.id.link_login);
-        btnsign=(buttons)findViewById(R.id.btn_signup);
+        etname = (edittext) findViewById(R.id.input_name);
+        etemail = (edittext) findViewById(R.id.input_email);
+        etpass = (edittext) findViewById(R.id.input_password);
+        loginlink = (textview) findViewById(R.id.link_login);
+        btnsign = (buttons) findViewById(R.id.btn_signup);
         btnsign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,16 +144,19 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
-    public void signup(){
+    public void signup() {
 
         if (!validate()) {
             onSignupFailed();
             return;
         }
         btnsign.setEnabled(false);
-        final ProgressDialog progressDialog=new ProgressDialog(SignupActivity.this,R.style.AppTheme2);
+        final ProgressDialog progressDialog = new ProgressDialog(SignupActivity.this, R.style.AppTheme2);
         progressDialog.setIndeterminate(true);
         progressDialog.setMessage("Creating Account");
         progressDialog.show();
@@ -77,9 +164,13 @@ public class SignupActivity extends AppCompatActivity {
         String email = etemail.getText().toString();
         String password = etpass.getText().toString();
 
-        // TODO: Signup code
-
-        new android.os.Handler().postDelayed(
+        HashMap<String,String> signupData = new HashMap<String,String>();
+        signupData.put("email",email);
+        signupData.put("p",encryptSHA512(password));
+        String response = new String();
+        response = performPostCall(link,signupData);
+        Log.i("response",response);
+        new Handler().postDelayed(
                 new Runnable() {
                     public void run() {
                         // On complete call either onSignupSuccess or onSignupFailed
@@ -103,7 +194,7 @@ public class SignupActivity extends AppCompatActivity {
             etname.setError(null);
         }
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             etemail.setError("enter a valid email address");
             valid = false;
         } else {
@@ -119,6 +210,7 @@ public class SignupActivity extends AppCompatActivity {
 
         return valid;
     }
+
     public void onSignupSuccess() {
         btnsign.setEnabled(true);
         setResult(RESULT_OK, null);
@@ -133,5 +225,45 @@ public class SignupActivity extends AppCompatActivity {
         Toast.makeText(getBaseContext(), "Sign-up failed", Toast.LENGTH_LONG).show();
 
         btnsign.setEnabled(true);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Signup Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.procleus.brime/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Signup Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.procleus.brime/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
