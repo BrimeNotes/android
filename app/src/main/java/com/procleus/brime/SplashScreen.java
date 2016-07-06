97511180adcc9c669c017f12a917a8506440dd05
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2016-07-06 11:47:10 +0530

    No Get Started page after First Start

--- app/src/main/java/com/procleus/brime/SplashScreen.java
+++ app/src/main/java/com/procleus/brime/SplashScreen.java
@@ -9,19 +9,29 @@ import android.transition.Fade;
 public class SplashScreen extends Activity {
 
     private final int SPLASH_DISPLAY_LENGTH = 1000;
+    SharedPreferences sharedPreferences = null;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
+        sharedPreferences = getSharedPreferences("com.procleus.brime", MODE_PRIVATE);
         setContentView(R.layout.activity_splash);
 
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                Intent mainIntent = new Intent(SplashScreen.this,IntroActivity.class);
-                startActivity(mainIntent);
-                SplashScreen.this.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                finish();
+                if (sharedPreferences.getBoolean("firstrun", true)) {
+                    Intent mainIntent = new Intent(SplashScreen.this,IntroActivity.class);
+                    startActivity(mainIntent);
+                    SplashScreen.this.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                    sharedPreferences.edit().putBoolean("firstrun", false).commit();
+                    finish();
+                }
+                else {
+                    Intent mainIntent = new Intent(SplashScreen.this,SigninActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
