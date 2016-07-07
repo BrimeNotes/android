e72c5c20f43b5fdc7c85cc87750a18bf7d3559ea
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2016-07-07 17:29:02 +0530

    Added Default Fragment

--- app/src/main/java/com/procleus/brime/MainActivity.java
+++ app/src/main/java/com/procleus/brime/MainActivity.java
@@ -59,6 +59,9 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        Fragment fragment = new DefaultFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.relativeLayout, fragment).commit();
     }
 
     @Override
