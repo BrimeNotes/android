6f72ba79cc4b8a2dc6df7846763475ab406ee11d
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2016-07-08 13:53:42 +0530

    Update LabelsFragment.java

--- app/src/main/java/com/procleus/brime/LabelsFragment.java
+++ app/src/main/java/com/procleus/brime/LabelsFragment.java
@@ -1,19 +1,38 @@
 package com.procleus.brime;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 /**
- * Created by Utkarsh on 07-07-2016.
+ * Created by Ujjwal on 07-07-2016.
  */
 public class LabelsFragment extends Fragment{
+
+    private static ListView listView;
+    public static String[] labels = new String[]{"Inspiration", "Personal", "Work"};
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.labels_gragment, container, false);
+        View v =inflater.inflate(R.layout.labels_gragment, container, false);
+        Button addLabel = (Button) v.findViewById(R.id.add_label);
+        addLabel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AddLabel.class);
+                startActivity(intent);
+            }
+        });
+        listView = (ListView)v.findViewById(R.id.listView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, labels);
+        listView.setAdapter(arrayAdapter);
+        return v;
     }
 }
