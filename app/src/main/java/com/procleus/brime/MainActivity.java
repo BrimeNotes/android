c08447954faf9d564a8838363f520d3bf534f529
syedsaad-h <hassan.saad.mail@gmail.com>
2016-07-09 13:30:43 +0530

    FAB added with animation

--- app/src/main/java/com/procleus/brime/MainActivity.java
+++ app/src/main/java/com/procleus/brime/MainActivity.java
@@ -3,7 +3,6 @@ package com.procleus.brime;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
@@ -11,32 +10,43 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
+        implements NavigationView.OnNavigationItemSelectedListener,View.OnClickListener {
+    private boolean isFabOpen = false;
     private static final int TIME_DELAY = 2000;
     private static long back_pressed;
+    private FloatingActionButton fab,fab1,fab2,fab3;
+    private Animation show_fab,hide_fab,rotate_fab_forward,rotate_fab_backward;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        // ===  FAB REFRENCES ===
+        fab = (FloatingActionButton)findViewById(R.id.fab);
+        fab1 = (FloatingActionButton)findViewById(R.id.fab_1);
+        fab2 = (FloatingActionButton)findViewById(R.id.fab_2);
+        fab3 = (FloatingActionButton)findViewById(R.id.fab_3);
+        // FAB Animations
+        show_fab = AnimationUtils.loadAnimation(getApplication(), R.anim.fab_open);
+        hide_fab = AnimationUtils.loadAnimation(getApplication(), R.anim.fab_close);
+        rotate_fab_forward = AnimationUtils.loadAnimation(getApplication(),R.anim.rotate_fab_forward);
+        rotate_fab_backward = AnimationUtils.loadAnimation(getApplication(),R.anim.rotate_fab_backward);
+        fab.setOnClickListener(this);
+        fab2.setOnClickListener(this);
+        fab3.setOnClickListener(this);
+        fab1.setOnClickListener(this);
+        // FAB END
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -50,6 +60,81 @@ public class MainActivity extends AppCompatActivity
         fragmentManager.beginTransaction().replace(R.id.relativeLayout, fragment).commit();
         getSupportActionBar().setTitle("Public Notes");
     }
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.fab:
+                animateFAB();
+                break;
+            case R.id.fab_1:
+
+                Log.d("Fab clicked ", "Fab 1");
+                break;
+            case R.id.fab_2:
+
+                Log.d("Fab Clicked ", "Fab 2");
+                break;
+        }
+    }
+
+    /* Function for Animation of Fab */
+    public void animateFAB(){
+
+        if(isFabOpen){
+
+            fab.startAnimation(rotate_fab_backward);
+            FrameLayout.LayoutParams layoutParamsFab1 = (FrameLayout.LayoutParams) fab1.getLayoutParams();
+            FrameLayout.LayoutParams layoutParamsFab2 = (FrameLayout.LayoutParams) fab2.getLayoutParams();
+            FrameLayout.LayoutParams layoutParamsFab3 = (FrameLayout.LayoutParams) fab3.getLayoutParams();
+            //layoutParams.rightMargin -= (int) (fab1.getWidth() * 1.7);
+            //layoutParams.bottomMargin -= (int) (fab1.getHeight() * 0.25);
+            //layoutParams.rightMargin -= (int) (fab1.getHeight() * 0.2);
+            layoutParamsFab1.bottomMargin -= (int) (fab1.getHeight() * 1.8);
+            fab1.setLayoutParams(layoutParamsFab1);
+            fab1.startAnimation(hide_fab);
+            fab1.setClickable(false);
+
+            layoutParamsFab2.bottomMargin -= (int) (fab2.getHeight() * 3);
+            fab2.setLayoutParams(layoutParamsFab2);
+            fab2.startAnimation(hide_fab);
+            fab2.setClickable(false);
+
+            layoutParamsFab3.bottomMargin -= (int) (fab3.getHeight() * 4.2);
+            fab3.setLayoutParams(layoutParamsFab3);
+            fab3.startAnimation(hide_fab);
+            fab3.setClickable(false);
+            isFabOpen = false;
+
+        } else {
+            fab.startAnimation(rotate_fab_forward);
+            FrameLayout.LayoutParams layoutParamsFab1 = (FrameLayout.LayoutParams) fab1.getLayoutParams();
+            FrameLayout.LayoutParams layoutParamsFab2 = (FrameLayout.LayoutParams) fab2.getLayoutParams();
+            FrameLayout.LayoutParams layoutParamsFab3 = (FrameLayout.LayoutParams) fab3.getLayoutParams();
+
+            layoutParamsFab1.rightMargin = (int) (fab1.getWidth() * 0.7);
+            layoutParamsFab1.bottomMargin += (int) (fab1.getHeight() * 1.8);
+            fab1.setLayoutParams(layoutParamsFab1);
+            fab1.startAnimation(show_fab);
+            fab1.setClickable(true);
+
+            layoutParamsFab2.rightMargin = (int) (fab2.getWidth() * 0.7);
+            layoutParamsFab2.bottomMargin += (int) (fab2.getHeight() * 3);
+            fab2.setLayoutParams(layoutParamsFab2);
+            fab2.startAnimation(show_fab);
+            fab2.setClickable(true);
+
+
+            layoutParamsFab3.rightMargin = (int) (fab3.getWidth() * 0.7);
+            layoutParamsFab3.bottomMargin += (int) (fab3.getHeight() * 4.2);
+            fab3.setLayoutParams(layoutParamsFab3);
+            fab3.startAnimation(show_fab);
+            fab3.setClickable(true);
+
+            isFabOpen = true;
+        }
+    }
+    /* Fab Animation Ends*/
 
     @Override
     public void onBackPressed() {
