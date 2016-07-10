49cf26adcaa03d4bfd435f596c5ec87026282458
maxsam4 <guptamudit@ymail.com>
2016-07-10 13:09:35 +0530

    textNote to TextNote

--- app/src/main/java/com/procleus/brime/textNote.java
+++ app/src/main/java/com/procleus/brime/textNote.java
@@ -5,14 +5,14 @@ import java.sql.Timestamp;
 /**
  * Created by mudit on 08/07/2016.
  */
-public class textNote {
+public class TextNote {
     public int id;
     public String note;
     public Timestamp created;
     public Timestamp edited;
     public int owner;
 
-    public textNote(int i, String n, Timestamp c, Timestamp e, int o) {
+    public TextNote(int i, String n, Timestamp c, Timestamp e, int o) {
         this.id = i;
         this.note = n;
         this.created = c;
