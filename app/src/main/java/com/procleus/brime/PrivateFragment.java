095d76571d1748e371224abd241ab75cdf603aed
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2016-07-10 14:55:07 +0530

    Update PrivateFragment.java

--- app/src/main/java/com/procleus/brime/PrivateFragment.java
+++ app/src/main/java/com/procleus/brime/PrivateFragment.java
@@ -1,5 +1,7 @@
 package com.procleus.brime;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,7 +10,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 /**
- * Created by Utkarsh on 07-07-2016.
+ * Created by Ujjwal on 07-07-2016.
  */
 public class PrivateFragment extends Fragment {
     final boolean isEmptyPrivate = true;
@@ -16,22 +18,29 @@ public class PrivateFragment extends Fragment {
     ImageView mImageView;	//reference to the ImageView
     int xDim, yDim;		//stores ImageView dimensions
     private bitmapCreate bitmap;
+    SharedPreferences sharedPreferences = null;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ((MainActivity) getActivity()).setActionBarTitle("Private Notes");
-        if(isEmptyPrivate == true){
-            view = inflater.inflate(R.layout.empty_notes, container, false);
-            mImageView = (ImageView)view.findViewById(R.id.empty_avatar);
-            xDim=300;
-            yDim=300;
-            mImageView.setImageBitmap(bitmap.decodeSampledBitmapFromResource(getResources(), R.drawable.empty_buddy, xDim, yDim));
-            return view;
+        sharedPreferences = this.getActivity().getSharedPreferences("com.procleus.brime", Context.MODE_PRIVATE);
+        Boolean bool = sharedPreferences.getBoolean("loggedin", false);
+        if (bool == false) {
+            // Alert Dialog
+        } else {
+            if(isEmptyPrivate == true){
+                view = inflater.inflate(R.layout.empty_notes, container, false);
+                mImageView = (ImageView)view.findViewById(R.id.empty_avatar);
+                xDim=300;
+                yDim=300;
+                mImageView.setImageBitmap(bitmap.decodeSampledBitmapFromResource(getResources(), R.drawable.empty_buddy, xDim, yDim));
+                return view;
+            }
+            else {
+                return inflater.inflate(R.layout.private_fragment, container, false);
+            }
         }
-        else {
-            return inflater.inflate(R.layout.private_fragment, container, false);
-        }
+        return inflater.inflate(R.layout.private_fragment, container, false);
     }
-
 }
