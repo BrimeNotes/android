ca1d94fdae4f4722e6216b3c1dacaeef0234e350
maxsam4 <guptamudit@ymail.com>
2016-07-10 15:40:09 +0530

    Insert note fixed

--- app/src/main/java/com/procleus/brime/CreateNotes.java
+++ app/src/main/java/com/procleus/brime/CreateNotes.java
@@ -2,11 +2,12 @@ package com.procleus.brime;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 
 public class CreateNotes extends AppCompatActivity {
-    EditText title;
-    EditText note;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,11 +16,11 @@ public class CreateNotes extends AppCompatActivity {
         getSupportActionBar().hide();
     }
 
-    public void create() {
+    public void create(View v) {
         Notes tn = new Notes(this);
-        setContentView(R.layout.activity_create_notes);
-        title = (EditText) findViewById(R.id.noteTitle);
-        note = (EditText) findViewById(R.id.note);
+        EditText title = (EditText) findViewById(R.id.noteTitle);
+        EditText note = (EditText) findViewById(R.id.note);
         tn.insertTextNote(note.getText().toString(), title.getText().toString(), 1);
+        Log.i(title.getText().toString(), note.getText().toString());
     }
 }
