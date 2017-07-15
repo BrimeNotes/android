ac3b42aca2a04306c625cd26ae7f4b500cafbb95
Ujjwal Bhardwaj <ujjwalb1996@gmail.com>
2017-07-16 00:58:36 +0530

    Add license header to core files

--- app/src/main/java/com/procleus/brime/ui/SettingsActivity.java
+++ app/src/main/java/com/procleus/brime/ui/SettingsActivity.java
@@ -1,3 +1,22 @@
+/**
+ * @author Suraj Rawat <suraj.raw120@gmail.com>
+ *
+ * @license AGPL-3.0
+ *
+ * This code is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
 package com.procleus.brime.ui;
 
 import android.os.Bundle;
@@ -6,11 +25,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.procleus.brime.R;
-
-
-/**
- * Created by suraj on 02-07-2017.
- */
 
 public class SettingsActivity extends AppCompatActivity{
 
